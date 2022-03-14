6b86bdf1a8b4aa270b16c768509e48df56f997e7
ShitalBhise <bhiseshital27059@gmail.com>
2022-03-14 22:37:47 +0530

    Validate Email address

--- java/junit/UserRegJunit.java
+++ java/junit/UserRegJunit.java
@@ -1,33 +1,42 @@
 package junit;
 
-import static org.junit.Assert.assertTrue;
-
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class UserRegJunit {
 	UserRegistration j = new UserRegistration();
-
+	/*
+	 * this Test case is used to check our first name passes given regex pattern
+	 */
 	@Test
 	public void test() {
 		boolean result = j.FirstName("Shital");
 
-		assertTrue(result);
+		assertEquals(true, result);
 
 	}
-
+	/* 
+	 * this Test case is used to check our last name passes given regex pattern 
+	 * 
+	 */
 	
 	@Test
 	public void testLastName() {
 		boolean result = j.LastName("bhisE");
 
-		asassertTrue(result);
+		assertEquals(true, result);
 
 	}
 
+	/* this Test case is used to check our email passes given regex pattern 
+	 */
+	@Test
+	public void testEmail() {
 
-	private void asassertTrue(boolean result) {
-		// TODO Auto-generated method stub
-		
+		boolean result = j.Email("xyz.abc@bl.co.in");
+		// assertTrue(result);
+		assertEquals(true, result);
 	}
+
 	
 }
--- java/junit/UserRegistration.java
+++ java/junit/UserRegistration.java
@@ -4,6 +4,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
+	private static final CharSequence Email = null;
 	public String regex;
 
 	public boolean FirstName(String firstName) {
@@ -25,5 +26,13 @@ public class UserRegistration {
 		return m.matches();
 
 	}
-	
+	public boolean Email(String lastName) {
+
+		regex = "[a-z]{2,}[A-Z]{1}";
+
+		Pattern p = Pattern.compile(regex);
+		Matcher m = p.matcher(Email);
+		return m.matches();
+
+	}
 }
